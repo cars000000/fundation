e4fe7e710c7f5f2da5bc61f7059c46f3cf812974
cars000000 <carsliu@sina.com>
2018-01-24 23:27:14 +0800

    2

--- fund_spider.py
+++ fund_spider.py
@@ -1,9 +1,10 @@
 import urllib3
 
+http_manager = urllib3.PoolManager()
 
 def get_fundation_history(fund_id, start_date=None, end_date=None):
     url_prefix = "http://www.dayfund.cn/fundvalue/"
-    url = url_prefix + fund_id
+    url = url_prefix + fund_id + ".html"
 
     if start_date is None:
         s_date = "2016-01-01"
@@ -11,9 +12,17 @@ def get_fundation_history(fund_id, start_date=None, end_date=None):
         s_date = start_date
 
     if end_date is None:
-        e_date = "2016-01-01"
+        e_date = "2018-01-01"
     else:
         e_date = end_date
 
-    urllib3.HTTPConnectionPool
-    result =
+    fields = {
+        "sdate": s_date,
+        "edate": e_date,
+    }
+
+    result = http_manager.request(method="GET", url=url, fields=fields)
+    print result.data
+
+if __name__ == '__main__':
+    get_fundation_history("5035")
